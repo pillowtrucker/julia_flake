970c2f6f7384d7acd9e60a8240857f3be755559d
pillowtrucker <githubino@janitor.chat>
2026-06-23 12:53:11 +0200

    ok but I actually need this in emacs probably

--- flake.nix
+++ flake.nix
@@ -43,6 +43,6 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell { packages = [ juliaFhsEnv ]; };
+        devShells.default = pkgs.mkShell { packages = [ juliaFhsEnv ]; shellHook="[[ -z $JULIA_DEPOT_PATH ]] || julia-fhs-cuda-plotting-env";};
       });
 }
